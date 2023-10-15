c58ca52259dd278e38be0d037c3fa51908b1ca13
ErikAmorim13 <erik.rodrigues@sptech.school>
2023-10-14 22:42:18 -0300

    Resolvendo merge

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,10 +5,10 @@ export const Button = styled.button`
     border: ${props => props.borda ? 'solid 1px #08632D': 'hidden'};
     color: ${props => props.color ? props.color : 'black'};
     height: 53px;
-    width: 161px;
+    width: ${props => props.width ? props.width : '161px'};
     border-radius: 5px;
     font-size: 13px;
     cursor: pointer;
-    font-weight:560;
+    font-weight:500;
 `
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,33 +1,28 @@
-import React from "react";
-import logo from "../../assets/logo2.png";
+import Header from './components/Header';
+import Input from './components/Input';
+import { Button } from '../../components/Button';
 import './login.css';
 
 function Login() {
   return (
     <div className="container">
-      <header className="header">
-        <img className="loginImg" src={logo} alt="Logo" />
-        <span>Bem Vindo</span>
-      </header>
+      <Header text="Bem Vindo!" />
+
       <form>
-        <div className="inputContainer">
-          <input type="text" name="email" id="email" required="required" />
-          <label htmlFor="email">E-mail</label>
-        </div>
+        <Input type="text" name="email" id="email" label="E-mail" />
+        <Input type="password" name="senha" id="senha" label="Senha" />
 
-        <div className="inputContainer">
-          <input type="password" name="senha" id="senha" required="required" />
-          <label htmlFor="senha">Senha</label>
-        </div>
-        <span  className="forgot-password">
-          Esqueceu a senha? <a href="/">Clique aqui</a>
-        </span>
+        <span>Esqueceu a senha? <a href="/">Clique aqui</a></span>
 
-        <button className="button">Entrar</button>
+        <Button bg="#08632D" color='white' width="100%">
+          Entrar
+        </Button>
+
         <div className="footer">
           <p>Não tem uma conta?</p>
           <a href="/">Inscreva-se</a>
         </div>
+
       </form>
     </div>
   );
