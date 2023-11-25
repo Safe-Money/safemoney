5254a7766c2d801af8e54a6908399a5ac9963965
GabrielSFerr <gabrielwwe211@gmail.com>
2023-11-24 22:11:12 -0300

    refactor: correções espaçamento

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export const Button = styled.button`
     background-color: ${props => props.bg};
     border: ${props => props.borda ? 'solid 1px #08632D': 'hidden'};
     color: ${props => props.color ? props.color : 'black'};
-    height: ${props => props.height ? props.height : '53px'};
+    height: ${props => props.height ? props.height : '50px'};
     width: ${props => props.width ? props.width : '161px'};
     border-radius: 5px;
     font-size: 13px;
