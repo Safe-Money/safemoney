8ba0048b458a24e2fa0739856bce4f01b05e8fee
NathanD4vid <nathan.silva@sptech.school>
2024-03-16 22:52:17 -0300

    hotfix - CORS

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5173',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [react()],
-})
+  server: {
+    cors: true,
+  },
+});
